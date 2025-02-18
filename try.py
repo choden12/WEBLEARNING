c957fc8d03aa3bb267f031d822ea78d8bce7ac04
choden12 <sangaycho27@gmail.com>
2025-02-18 18:04:25 +0600

    try

---
+++ try.py
@@ -0,0 +1,9 @@
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate sum
+sum_result = num1 + num2
+
+# Print result
+print("The sum is:", sum_result)
